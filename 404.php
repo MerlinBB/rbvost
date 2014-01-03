c4cda28a261af41b120454c9021673e350f0e66c
Merlin Mason <info@merlinmason.co.uk>
2014-01-03 23:00:13 +0000

    first commit

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<article class="fourohfour">
+    <h1>Dang, we're lost!</h1>
+    <p>There's been a 404 error. Go <a href="/">home</a>?</p>
+</article>
+
+<?php get_footer(); ?>
