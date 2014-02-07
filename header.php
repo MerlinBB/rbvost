d18c736f7b40554eed96638d4ead26b672a00014
Merlin Mason <info@merlinmason.co.uk>
2014-02-07 23:20:03 +0000

    include redbull header and assets

--- header.php
+++ header.php
@@ -17,11 +17,25 @@
     <title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
     <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>"/>
+
+    <?php // RB global footer styles and fonts ?>
+    <link href="https://create.redbull.com/cs/RedBull2/stylesheets/shared-modules/shared-footer-20130829100721.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="https://create.redbull.com/cs/RedBull2/stylesheets/shared-modules/shared-footer-desktop-20130829100721.css" media="only screen and (min-width: 640px)" rel="stylesheet" type="text/css" />
+    <!--[if lte IE 8]>
+      <link href="/cs/RedBull2/stylesheets/shared-modules/shared-footer-IE-lte8-20130829100721.css" media="screen" rel="stylesheet" type="text/css" />
+    <![endif]-->
+
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>"/>
-    <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon.ico">
+    <link rel="icon" type="image/png" href="<?php bloginfo('template_url'); ?>/img/favicon.png">
 
     <?php wp_head(); ?>
 
 </head>
 
 <body <?php body_class(); ?>>
+
+<?php include("_redbullheader.html"); ?>
+
+<div class="alert hidden">
+    Sorry, you've entered an incorrect username or password.
+</div>
