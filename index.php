0e7ff5937359e0ccc18141f4578fbe6189ac1742
Merlin Mason <info@merlinmason.co.uk>
2014-02-07 23:19:35 +0000

    conditionally show login form

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
     get_header();
 
-        echo '<div id=app></div>';
+        if ( is_user_logged_in() ) {
+
+            echo '<div id=app></div>';
+
+        } else {
+
+            echo '<div class="login-container">';
+                $args = array(
+                    'echo' => true,
+                    'redirect' => site_url( '/' ),
+                    'label_log_in' => __( 'Login' ),
+                    'value_username' => NULL
+                );
+                wp_login_form( $args );
+            echo "</div>";
+
+        }
 
     get_footer();
 ?>
