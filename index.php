c44c5ba277dbc10936b2dcd774978cf03d425d7c
Merlin Mason <info@merlinmason.co.uk>
2014-02-17 21:06:48 +0000

    update to multimonth version of clndr

--- index.php
+++ index.php
@@ -3,32 +3,18 @@
 
         if ( is_user_logged_in() ) { ?>
 
-            <div id="app">
+            <div id="app" class="container">
                 <nav>
+                    <div class="year-selector-view fl"></div>
                     <button data-navigate="campaign">Campaigns</button>
                     <button data-navigate="calendar">Calendar</button>
                 </nav>
-                <div class="year-selector-view"></div>
 
                 <div class="switch-view campaign-filters-view"></div>
                 <div class="switch-view campaign-list-view"></div>
 
                 <div class="switch-view calendar-filters-view"></div>
-
-                <div class="switch-view calendar-view">
-                    <div class="calendar-subview-1"></div>
-                    <div class="calendar-subview-2"></div>
-                    <div class="calendar-subview-3"></div>
-                    <div class="calendar-subview-4"></div>
-                    <div class="calendar-subview-5"></div>
-                    <div class="calendar-subview-6"></div>
-                    <div class="calendar-subview-7"></div>
-                    <div class="calendar-subview-8"></div>
-                    <div class="calendar-subview-9"></div>
-                    <div class="calendar-subview-10"></div>
-                    <div class="calendar-subview-11"></div>
-                    <div class="calendar-subview-12"></div>
-                </div>
+                <div class="switch-view calendar-view"></div>
             </div>
 
         <?php } else {
